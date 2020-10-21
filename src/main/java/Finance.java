82595079e0da18706fbf3d31057c0b62104cf966
Cédric Capes <cedric.capes@capgemini.com>
2020-10-21 12:00:07 +0200

    Module 5 done

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -10,11 +10,10 @@ public class Finance {
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String, String> commandsToUsage(){
-        return Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
+    public final static Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
                 SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
                 MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
-    }
+
 
     private static boolean validateCommandArguments(String[] args){
         switch (args[0]){
